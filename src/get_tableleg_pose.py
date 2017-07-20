b995df187aa9d93f67c1b861712be03f9364dac2
srinjoym <srinjoy.majumdar@datos.io>
2017-07-20 12:14:11 -0700

    adding arm move fucntionality

--- src/get_tableleg_pose.py
+++ src/get_tableleg_pose.py
@@ -2,24 +2,38 @@
 
 from tableleg_pose.srv import *
 import geometry_msgs.msg
+from arm_moveit import *
+
 import rospy
 
+class GetTableLegPose:
 
-def handle_get_tableleg_pose(req):
-    # print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b))
-    tableleg_pose = req.gripper_pose
-
-    tableleg_pose.position.x += 5
-
-    return TableLegPoseResponse(req.gripper_pose)
+    def __init__(self):
+        self.arm = ArmMoveIt()
 
 
-def get_tableleg_pose_server():
-    rospy.init_node('get_tableleg_pose_server')
-    s = rospy.Service('get_tableleg_pose', TableLegPose, handle_get_tableleg_pose)
-    print "Ready to output table leg pose."
-    rospy.spin()
+
+
+    def handle_get_tableleg_pose(self,req):
+        # print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b))
+
+        self.arm.group[0].set_pose_target(req.gripper_pose)
+
+        self.arm.group[0].go()
+
+        tableleg_pose = req.gripper_pose
+
+        tableleg_pose.position.x += 5
+
+        return TableLegPoseResponse(req.gripper_pose)
+
+
+    def get_tableleg_pose_server(self):
+        rospy.init_node('get_tableleg_pose_server')
+        s = rospy.Service('get_tableleg_pose', TableLegPose, self.handle_get_tableleg_pose)
+        print "Ready to output table leg pose."
+        rospy.spin()
 
 
 if __name__ == "__main__":
-    get_tableleg_pose_server()
+    GetTableLegPose().get_tableleg_pose_server()
